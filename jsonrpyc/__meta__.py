44a0477256f039ff91e16ba554afe5f6d49238a8
Marcel R. <github.riga@icloud.com>
2023-11-22 18:20:01 +0100

    Refactor.

---
+++ jsonrpyc/__meta__.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+
+"""
+Minimal python RPC implementation in a single file based on the JSON-RPC 2.0 specs from
+http://www.jsonrpc.org/specification.
+"""
+
+__author__ = "Marcel Rieger"
+__email__ = "github.riga@icloud.com"
+__copyright__ = "Copyright 2016-2023, Marcel Rieger"
+__credits__ = ["Marcel Rieger"]
+__contact__ = "https://github.com/riga/jsonrpyc"
+__license__ = "BSD-3-Clause"
+__status__ = "Development"
+__version__ = "1.1.1"
