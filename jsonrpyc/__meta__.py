a8f428e9962829a4e1706ff7241a88a49a46e115
Marcel R. <github.riga@icloud.com>
2023-11-22 19:06:28 +0100

    Version 1.2.1.

--- jsonrpyc/__meta__.py
+++ jsonrpyc/__meta__.py
@@ -12,4 +12,4 @@ __credits__ = ["Marcel Rieger"]
 __contact__ = "https://github.com/riga/jsonrpyc"
 __license__ = "BSD-3-Clause"
 __status__ = "Development"
-__version__ = "1.2.0"
+__version__ = "1.2.1"
